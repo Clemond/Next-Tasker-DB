24b9a0090f9d910598d10c29b1b658ab579cbe87
Nicholas <quackshows11@gmail.com>
2024-12-17 15:03:42 +0100

    fix: CORS issues between frontend and backend

---
+++ src/main/kotlin/com/clemond/next_tasker_db/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.clemond.next_tasker_db.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // Apply to all endpoints
+            .allowedOrigins("http://localhost:3000") // Allow specific origin
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
+            .allowedHeaders("*") // Allowed headers
+    }
+}
