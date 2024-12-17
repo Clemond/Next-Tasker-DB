a004714feaee316442d7382821c2b228e6aad784
Nicholas <quackshows11@gmail.com>
2024-12-17 13:00:03 +0100

    feat: add CustomUserRepository and CustomUserController with user saving endpoint

---
+++ src/main/kotlin/com/clemond/next_tasker_db/user/controller/CustomUserController.kt
@@ -0,0 +1,32 @@
+package com.clemond.next_tasker_db.user.controller
+
+import com.clemond.next_tasker_db.user.model.CustomUser
+import com.clemond.next_tasker_db.user.repository.CustomUserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/user")
+class CustomUserController (
+    @Autowired val customUserRepository: CustomUserRepository
+    // TODO - Add passwordEncoder
+){
+
+    @PostMapping
+    fun saveUser(
+        @RequestBody newUser: CustomUser
+    ): ResponseEntity<String> {
+
+        customUserRepository.save(newUser)
+
+        return ResponseEntity.status(201).body("user was successfully created")
+
+    }
+
+
+}
---
+++ src/main/kotlin/com/clemond/next_tasker_db/user/repository/CustomUserRepository.kt
@@ -0,0 +1,7 @@
+package com.clemond.next_tasker_db.user.repository
+
+import com.clemond.next_tasker_db.user.model.CustomUser
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CustomUserRepository: JpaRepository<CustomUser, Long> {
+}
