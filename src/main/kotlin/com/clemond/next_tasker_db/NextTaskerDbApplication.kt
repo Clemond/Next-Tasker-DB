0e9f76736440e8918f4db29eeaa56feeab45489d
Nicholas <quackshows11@gmail.com>
2024-12-17 10:34:28 +0100

    initial commit (added application.properties to gitignore)

---
+++ src/main/kotlin/com/clemond/next_tasker_db/NextTaskerDbApplication.kt
@@ -0,0 +1,11 @@
+package com.clemond.next_tasker_db
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextTaskerDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<NextTaskerDbApplication>(*args)
+}
---
+++ src/test/kotlin/com/clemond/next_tasker_db/NextTaskerDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clemond.next_tasker_db
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextTaskerDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
